12cbdb7b3700d4ee4883b0af3056e43dee3ce838
Kasparas Z <kasparaszuj@gmail.com>
2020-02-25 13:51:24 +0200

    Updated to meet Lab3 requirements

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function openFormUp() {
+  document.getElementById("myFormUp").style.display = "block";
+}
+
+function openFormIn() {
+  document.getElementById("myFormIn").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myFormIn").style.display = "none";
+    document.getElementById("myFormUp").style.display = "none";
+}
+
+function background1() {
+    document.body.style.backgroundImage  = "url('../lab2/img/1.jpg')";
+}
+function background2() {
+    document.body.style.backgroundImage  = "url('../lab2/img/2.jpg')";
+}
+function background3() {
+    document.body.style.backgroundImage  = "url('../lab2/img/3.jpg')";
+}
